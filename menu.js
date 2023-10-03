bae822132cabcb0e390c51012ff1922c02bfffc5
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-10-03 19:12:59 +0200

    Changed buttons on menu

--- menu.js
+++ menu.js
@@ -3,24 +3,26 @@ document.getElementById('play-button').addEventListener('click', function () {
 });
 
 document.getElementById('controls-button').addEventListener('click', function () {
-    alert('Controls: WASD to move, SPACE to jump, MOUSE to look around.');
+    window.location.href = 'controls.html';
+});
+
+document.getElementById('settings-button').addEventListener('click', function () {
+    window.location.href = 'settings.html';
 });
 
 document.getElementById('about-button').addEventListener('click', function () {
-    // Add code to show game information or navigate to the about page
-    alert('About the Game: This is a three.js demo with basic collisions.');
+    window.location.href = 'about.html';
 });
 
-// Get a reference to the audio element
 const backgroundMusic = new Audio('music/menusong.mp3');
-backgroundMusic.loop = true; // Make the audio loop continuously
+backgroundMusic.loop = true;
 
-// Add a click event listener to the Bluetooth switch
+
 document.getElementById('music').addEventListener('change', function () {
     if (this.checked) {
-        backgroundMusic.play(); // Play the audio
+        backgroundMusic.play();
     } else {
-        backgroundMusic.pause(); // Pause the audio
+        backgroundMusic.pause(); 
     }
 });
 
