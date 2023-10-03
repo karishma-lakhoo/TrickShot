0215901de110e2c9a9662ac3c7c88c77c63bbc9a
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-10-03 18:08:01 +0200

    added music

--- menu.js
+++ menu.js
@@ -10,3 +10,18 @@ document.getElementById('about-button').addEventListener('click', function () {
     // Add code to show game information or navigate to the about page
     alert('About the Game: This is a three.js demo with basic collisions.');
 });
+
+// Get a reference to the audio element
+const backgroundMusic = new Audio('music/menusong.mp3');
+backgroundMusic.loop = true; // Make the audio loop continuously
+
+// Add a click event listener to the Bluetooth switch
+document.getElementById('music').addEventListener('change', function () {
+    if (this.checked) {
+        backgroundMusic.play(); // Play the audio
+    } else {
+        backgroundMusic.pause(); // Pause the audio
+    }
+});
+
+
